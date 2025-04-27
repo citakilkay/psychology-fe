cd2655595d5b3baa19817b7b76eb0ea2338b1689
Ilkay Citak <citakilkay@gmail.com>
2025-04-27 16:45:11 +0300

    min fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import MainLayout from './components/layout/index.tsx'
+import MainLayout from './components/layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
